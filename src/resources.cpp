f8f6d0d2e3e2a21bf48e0ae131015e2542f0f840
arngo <27396817+arngo@users.noreply.github.com>
2022-11-02 14:28:51 -0400

    implement resource loader

---
+++ src/resources.cpp
@@ -0,0 +1,27 @@
+#include <unordered_map>
+#include <string>
+#include <stdexcept>
+#include "raylib.h"
+#include "resources.h"
+
+namespace Resources {
+    static std::unordered_map<std::string, std::string> textureFiles = {
+        {"ship", "./assets/textures/spaceShooter2_spritesheet_2X.png"}
+    };
+    static std::unordered_map<std::string, Texture> textures;
+    Texture useTexture(std::string id) {
+        Texture texture;
+        auto texSearch = textures.find(id);
+        if (texSearch != textures.end()) {
+            return texSearch->second;
+        }
+        auto texPathSearch = textureFiles.find(id);
+        if (texPathSearch != textureFiles.end()) {
+            texture = LoadTexture(texPathSearch->second.c_str());
+            textures.insert({id, texture});
+        } else {
+            throw std::invalid_argument("Texture id '" + id + "' has no assigned path.");
+        }
+        return texture;
+    }
+}
---
+++ src/resources.h
@@ -0,0 +1,7 @@
+#include <unordered_map>
+#include <string>
+#include "raylib.h"
+
+namespace Resources {
+    Texture useTexture(std::string id);
+}
